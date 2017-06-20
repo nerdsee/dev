432a91a703fadd5b2e2a318c511279db72ffcbf5
nerdsee <jan@stoevesand.org>
2017-06-20 22:46:03 +0200

    logging 3

--- findow/src/org/stoevesand/findow/server/FindowSystem.java
+++ findow/src/org/stoevesand/findow/server/FindowSystem.java
@@ -1,5 +1,7 @@
 package org.stoevesand.findow.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.stoevesand.findow.model.FinErrorHandler;
 import org.stoevesand.findow.model.FinUser;
 import org.stoevesand.findow.provider.BankingAPI;
@@ -8,13 +10,16 @@ import org.stoevesand.findow.provider.finapi.FinapiBankingAPI;
 
 public class FindowSystem {
 
+	private static Logger log = LoggerFactory.getLogger(FindowSystem.class);
+
 	private static final String FINAPI = "FINAPI";
 	private static final String FIGO = "FIGO";
 
 	public static BankingAPI getBankingAPI(FinUser user) throws FinErrorHandler {
-		if (user==null) {
+		if (user == null) {
+			log.error("na valid user to get the BankingAPI (null).");
 			throw new FinErrorHandler("Not a valid user. (null)");
-		} 
+		}
 		return getBankingAPI(user.getApi());
 	}
 
